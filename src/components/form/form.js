bd460e2ffe8056f46e19158bb9fa9d46c6324567
Jose Padilla <jose.apadilla99@gmail.com>
2023-01-23 20:05:43 -0600

    feat: input field with button co bh button component added

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -5,7 +5,7 @@ const Form = (props) => (
 
     <form>
         <label>{props.label}</label>
-        <input className={`input-${props.size}`} placeholder={props.placeholder || props.label}></input>
+        <input className={`input-${props.size}`} placeholder={props.placeholder || props.label} id="inputForm"></input>
     </form>
 )
 
--- src/components/inputButton/inputButton.js
+++ src/components/inputButton/inputButton.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./inputButton.css"
-import Select from "./select"
-import button from "./button"
+import Form from "../form/form.js"
+import "../form/form.css"
+import Button from "../button/button.js"
+import "../button/button.css"
 
-const inputButton = () => (
-    <div>
-        <Select />
-        <Button />
+function InputButton (props) {
+    function clicked () {
+        let formInput = document.getElementById("inputForm").value
+        alert(formInput)
+    }
+    return (
+    <div >
+        <Form placeholder={props.placeholder} size={props.size} />
+        <Button label={props.label} type="primary" size={props.size} action={clicked}/>
     </div>
-)
+    )
+}
+
+export default InputButton
